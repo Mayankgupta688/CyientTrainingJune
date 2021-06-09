cce0a7766ed671bae9839214cf19d1cdf7e5e1e5
MayankGupta <mayank.gupta.6.88@gmail.com>
2021-06-09 13:26:29 +0530

    Session Three

--- employee-management/src/app.module.ts
+++ employee-management/src/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { SalutationComponent } from "./components/salutation/salutation.component"
+import { SalutationComponent } from "./components/salutation/salutation.component";
+import { TrainingDetailsComponent } from "./components/trainingDetails/trainingDetails.component";
+import { AppStartComponent } from "./container/appStart/app.start.component";
+import { DataBindingComponent } from "./components/dataBinding/data.binding.component";
+import { EmployeeInfoComponent } from "./components/employeeInfo/employee.info.component";
 
 @NgModule({
-   declarations: [ SalutationComponent ],
+   declarations: [ 
+      SalutationComponent, 
+      TrainingDetailsComponent, 
+      AppStartComponent,
+      DataBindingComponent,
+      EmployeeInfoComponent 
+   ],
    imports: [ BrowserModule ],
-   bootstrap: [ SalutationComponent ]
+   bootstrap: [ EmployeeInfoComponent ]
 })
 export class ApplicationModule {}
--- employee-management/src/components/salutation/salutation.component.ts
+++ employee-management/src/components/salutation/salutation.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core"
 
 @Component({
-    selector: "salution",
+    selector: "salutation",
     templateUrl: "./salutation.component.html",
     styleUrls: ["./salutation.component.css"]
 })
